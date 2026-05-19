dfb9991454e6d92d431960a8e04a79d40c412a09
alizubu <azzubair1470@gmail.com>
2026-05-19 15:33:05 +0600

    v1.1.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { ThemeProvider } from "../components/ThemeProvider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
+// ⚠️ সরাসরি next-themes থেকে নয়, আমাদের বানানো ক্লায়েন্ট প্রোভাইডার ইম্পোর্ট করতে হবে
+import { ThemeProvider } from "../components/ThemeProvider"; 
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
-  title: "alizubu Quote Maker",
+  title: "Alizubu Quote Maker",
   description: "Professional Web-based Canvas Editor",
 };
 
@@ -21,10 +22,15 @@ export default function RootLayout({
     // suppressHydrationWarning দেওয়া হয়েছে যাতে next-themes এর কারণে কোনো ওয়ার্নিং না আসে
     <html lang="en" suppressHydrationWarning className={cn("font-sans", inter.variable)}>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider 
+          attribute="class" 
+          defaultTheme="dark" 
+          enableSystem 
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
